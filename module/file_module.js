332a5ba0d6968d22777d087fce0c8bd2b98ce31b
Yash Dodani <yashdodani26@gmail.com>
2025-01-05 22:13:30 +0530

    1/2 done !!!

--- module/file_module.js
+++ module/file_module.js
@@ -35,6 +35,75 @@ fs.appendFile('mynewFile1.txt' , 'hello World' , function(err){
 
 
 
-//create file with writiteing
+//create file with writiteing using open fuction
+
+/*
+fs.open('mynewfile2.txt' , 'r' , function (err , file){
+    if(err) throw err;
+    console.log("Saved !!");
+});
+*/
 
 
+//The fs.writeFile() method replaces the specified file and content if it exists. 
+// If the file does not exist, a new file, containing the specified content, will be created.
+
+
+/*
+fs.writeFile('mynewfile3.txt' , "Hello It is my Content !!! " , function(err){
+    if (err) {
+        throw err
+    }
+    console.log("Saved !!!");
+});
+
+*/
+
+
+
+//Update File
+
+
+
+//The fs.appendFile() method appends the specified content at the end of the specified file:
+
+
+
+/*
+let str = "\n Update line "
+for(i=1;i<=100;i++)
+    {
+            str += i;
+
+    }
+    console.log(str);
+fs.appendFile('mynewfile1.txt' ,str, function(err){
+    
+    if(err) throw err;
+   
+});
+
+*/
+
+
+//he fs.writeFile() method replaces the specified file and content:
+/*
+fs.writeFile('mynewfile3.txt' , "This is my text" , function(err){
+    if(err) throw err;
+    console.log("Replace!!!");
+});
+
+*/
+
+
+
+//Delete Files
+
+
+//Using fs.unlink()
+
+
+fs.unlink('mynewfile1.txt',function(err){
+    if(err) throw err;
+    console.log("deleted!!!");
+});
